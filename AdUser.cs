f6e84c836e6ef29a08dc17c3cd703142b142d9fb
wase932 <wase932@gmail.com>
2019-03-28 15:09:16 -0700

    All fields to retrieve user information

---
+++ AdUser.cs
@@ -0,0 +1,100 @@
+using System.Linq;
+using System;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+
+namespace ActiveDirectory
+{
+    class AdUser
+    {
+        public string department { get; set; }
+        public string badpasswordtime { get; set; }
+        public string msexchuserculture { get; set; }
+        public string distinguishedname { get; set; }
+        public string extensionattribute12 { get; set; }
+        public string samaccounttype { get; set; }
+        public string msexchelcmailboxflags { get; set; }
+        public string cn { get; set; }
+        public string msexchpoliciesincluded { get; set; }
+        public string proxyaddresses { get; set; }
+        public string msexcharchivename { get; set; }
+        public string extensionattribute11 { get; set; }
+        public string otherpager { get; set; }
+        public string employeeid { get; set; }
+        public string homedrive { get; set; }
+        public string initials { get; set; }
+        public string showinaddressbook { get; set; }
+        public string objectsid { get; set; }
+        public string msexchumdtmfmap { get; set; }
+        public string lastlogontimestamp { get; set; }
+        public string adspath { get; set; }
+        public string homedirectory { get; set; }
+        public string dscorepropagationdata { get; set; }
+        public string whenchanged { get; set; }
+        public string l { get; set; }
+        public string msexcharchiveguid { get; set; }
+        public string lastlogoff { get; set; }
+        public string userprincipalname { get; set; }
+        public string company { get; set; }
+        public string postalcode { get; set; }
+        public string lastlogon { get; set; }
+        public string managedobjects { get; set; }
+        public string sidhistory { get; set; }
+        public string pwdlastset { get; set; }
+        public string countrycode { get; set; }
+        public string logoncount { get; set; }
+        public string employeenumber { get; set; }
+        public string badpwdcount { get; set; }
+        public string mobile { get; set; }
+        public string memberof { get; set; }
+        public string description { get; set; }
+        public string displayname { get; set; }
+        public string msexchsafesendershash { get; set; }
+        public string msexchrecipienttypedetails { get; set; }
+        public string extensionattribute4 { get; set; }
+        public string c { get; set; }
+        public string accountnamehistory { get; set; }
+        public string usncreated { get; set; }
+        public string msexchversion { get; set; }
+        public string objectclass { get; set; }
+        public string legacyexchangedn { get; set; }
+        public string sn { get; set; }
+        public string msexchhidefromaddresslists { get; set; }
+        public string mail { get; set; }
+        public string accountexpires { get; set; }
+        public string usnchanged { get; set; }
+        public string extensionattribute3 { get; set; }
+        public string name { get; set; }
+        public string employeetype { get; set; }
+        public string samaccountname { get; set; }
+        public string givenname { get; set; }
+        public string targetaddress { get; set; }
+        public string objectguid { get; set; }
+        public string telephonenumber { get; set; }
+        public string streetaddress { get; set; }
+        public string codepage { get; set; }
+        public string msexchblockedsendershash { get; set; }
+        public string logonhours { get; set; }
+        public string msexchremoterecipienttype { get; set; }
+        public string userparameters { get; set; }
+        public string thumbnailphoto { get; set; }
+        public string useraccountcontrol { get; set; }
+        public string st { get; set; }
+        public string title { get; set; }
+        public string physicaldeliveryofficename { get; set; }
+        public string msexchmailboxguid { get; set; }
+        public string co { get; set; }
+        public string extensionattribute9 { get; set; }
+        public string whencreated { get; set; }
+        public string instancetype { get; set; }
+        public string objectcategory { get; set; }
+        public string authorigbl { get; set; }
+        public string manager { get; set; }
+        public string msexchrecipientdisplaytype { get; set; }
+        public string directreports { get; set; }
+        public string ipphone { get; set; }
+        public string primarygroupid { get; set; }
+        public string mailnickname { get; set; }
+
+    }
+}
