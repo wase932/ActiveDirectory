6f1c042e33851844aaa3692588e83ad9fc6fff76
wase932 <wase932@gmail.com>
2019-03-28 12:24:57 -0700

    Added the windows compactibility pack https://www.nuget.org/packages/Microsoft.Windows.Compatibility

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ActiveDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PrincipalContext(ContextType.Domain, "dcs.azdcs.gov");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
